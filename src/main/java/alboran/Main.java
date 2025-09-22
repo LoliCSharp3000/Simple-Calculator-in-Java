07c53298c489f5629f8c28629587544216dede6c
LoliCSharp3000 <araara3090ti@gmail.com>
2025-09-22 11:08:06 +0200

    Terminado

--- src/main/java/alboran/Main.java
+++ src/main/java/alboran/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int Election = 0, Value1, Value2, Result;
         System.out.println("Que quieres hacer? 1.Sumar   2.Restar  3.multiplicar  4.dividir   5.Salir");
         
-        for(;Election == 5;){
+        while(Election != 5){
             Election = sc.nextInt();
             switch (Election) {
             case 1:
@@ -18,7 +18,7 @@ public class Main {
                 System.out.println("Dime el segundo valor");
                 Value2= sc.nextInt();
                 Result = Value1 + Value2;
-                System.err.println("Vuestro resultado es " + Result);
+                System.out.println("Vuestro resultado es " + Result);
                 break;
             case 2:
                 System.out.println("Dime el primer valor");
@@ -26,7 +26,7 @@ public class Main {
                 System.out.println("Dime el segundo valor");
                 Value2= sc.nextInt();
                 Result = Value1 - Value2;
-                System.err.println("Vuestro resultado es " + Result);
+                System.out.println("Vuestro resultado es " + Result);
                 break;
             case 3:
                 System.out.println("Dime el primer valor");
@@ -34,21 +34,29 @@ public class Main {
                 System.out.println("Dime el segundo valor");
                 Value2= sc.nextInt();
                 Result = Value1 * Value2;
-                System.err.println("Vuestro resultado es " + Result);
+                System.out.println("Vuestro resultado es " + Result);
                 break;
             case 4:
                 System.out.println("Dime el primer valor");
                 Value1 = sc.nextInt();
                 System.out.println("Dime el segundo valor");
                 Value2= sc.nextInt();
-                Result = Value1 / Value2;
-                System.err.println("Vuestro resultado es " + Result);
+                if(Value2 != 0){
+                    Result = Value1 / Value2;
+                    System.out.println("Vuestro resultado es " + Result);
+                }else{
+                    System.out.println("No podeis dividir entre 0");
+                }
+                
+                break;
+            case 5:
+                System.out.println("Saliendo de la calculadora");
                 break;
             default:
-                System.err.println("No se a elegido correctamente");
-                throw new AssertionError();
+                System.out.println("No se a elegido correctamente");
+                break;
+            }
         }
-        }
-        
+        sc.close();
     }
 }
